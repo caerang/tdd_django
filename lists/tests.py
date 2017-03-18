7a48d02a57a62325b82e3f5dc70eb28c714a37a1
LEEHYEONG DO <hyeongdo@LEEHYEONGui-MacBook-Pro.local>
2017-03-18 18:30:00 +0900

    POST 처리 작업 중

--- lists/tests.py
+++ lists/tests.py
@@ -5,8 +5,21 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
+import re
+
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,9 +28,22 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string(
+            'home.html',
+            # {'new_item_text': '신규 작업 아이템'},
+            #request=request
+        )
+        self.assertEqualExceptCSRF(response.content.decode(), expected_html)
 
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
