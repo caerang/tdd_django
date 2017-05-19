836d118092b4f7d77dfa281d0362ba7d33697795
LEEHYEONG DO <hyeongdo@LEEHYEONGui-MacBook-Pro.local>
2017-05-19 19:09:47 +0900

    Modify Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -29,26 +29,22 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string(
-            'home.html',
-            # {'new_item_text': '신규 작업 아이템'},
-            #request=request
-        )
+        expected_html = render_to_string('home.html')
         self.assertEqualExceptCSRF(response.content.decode(), expected_html)
 
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
 
-    def test_home_page_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
-
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    # def test_home_page_can_save_a_POST_request(self):
+    #     response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
+    #
+    #     self.assertIn('신규 작업 아이템', response.content.decode())
+    #     self.assertTemplateUsed(response, 'home.html')
+    #
+    # def test_uses_home_template(self):
+    #     response = self.client.get('/')
+    #     self.assertTemplateUsed(response, 'home.html')
 
 
 class ItemModelTest(TestCase):
@@ -69,3 +65,31 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        # self.assertIn('A new list item', response.content.decode())
+        # self.assertTemplateUsed(response, 'home.html')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
