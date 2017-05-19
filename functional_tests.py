4c9649cff33e24a3139adbc1d3cca97a70465c80
LEEHYEONG DO <hyeongdo@LEEHYEONGui-MacBook-Pro.local>
2017-05-19 18:25:14 +0900

    Add Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -10,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -25,16 +31,20 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('공작깃털 사기')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('날 수 있도록 공작깃털 사용하기')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn('2: 날 수 있도록 공작깃털 사용하기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.check_for_row_in_list_table('2: 날 수 있도록 공작깃털 사용하기')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        # self.assertIn('2: 날 수 있도록 공작깃털 사용하기', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
