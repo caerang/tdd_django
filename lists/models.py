723c52ccbe139eddc4a3d5905590b39a60ec40fd
HYEONG DO LEE <hyeongdolee@HYEONGui-MacBook-Pro.local>
2017-06-07 19:28:10 +0900

    Finish 7.9 Biting the bullet: Adjusting Our Models

--- lists/models.py
+++ lists/models.py
@@ -6,3 +6,8 @@ class Item(models.Model):
     Item Model
     """
     text = models.TextField(default='')
+    list = models.ForeignKey('List', default=None)
+
+
+class List(models.Model):
+    pass
